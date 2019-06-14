7c2fd17ae20ce10020ae4f0b4617176970591286
Tigermouthbear <46039644+Tigermouthbear@users.noreply.github.com>
2019-06-14 15:34:17 -0400

    Add files via upload

---
+++ q.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+mc_folder = r"C:\Users\bearw\AppData\Roaming\.minecraft"
+
+latest_log = mc_folder + r"\logs\latest.log"
+
+os.startfile(r"C:\Apache24\bin\httpd.exe")
+
+while True:
+    with open(latest_log, "rb") as f:
+        f.seek(-2, os.SEEK_END)     
+        while f.read(1) != b"\n":
+            f.seek(-2, os.SEEK_CUR)
+        latest = f.readline()
+        latest = str(latest)
+        if 'CHAT' in latest:
+            os.remove(r"C:\Apache24\htdocs\index.html")
+            p = open("C:\Apache24\htdocs\index.html","w+")
+            p.write("<meta http-equiv='refresh' content='10'><div style='text-align: center;'><h1>" + latest[32:-5] + "</h1></div>")
+            p.close()
+            print(latest[51:-5])
+    time.sleep(0.5)
